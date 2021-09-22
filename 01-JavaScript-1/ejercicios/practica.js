81c64dddcd9783be582ddbb132be57b3cd58802e
Carla Salome Serrano <c.salome.serrano@gmail.com>
2021-09-22 09:37:47 -0300

    primer commit

--- 01-JavaScript-1/ejercicios/practica.js
+++ 01-JavaScript-1/ejercicios/practica.js
@@ -228,12 +228,15 @@ function esVocal(letra) {
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
-  if ( letra = a || letra = e || letra = i || letra = o || letra u ) {
+  if ( letra == 'a'|| letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u' ) {
     return 'Es vocal'
-  } else { return 'Dato incorrecto'
-    
-  }
+  } else if (letra.length > 1 ) {
+     return 'Dato incorrecto'}
+     else {
+       return 'Dato incorrecto'
+     }
+
+  
 }
 
 // No modificar nada debajo de esta línea
